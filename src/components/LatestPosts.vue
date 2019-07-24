73664c14dcb2fbcfeb2b0611f38c73a394e3ea39
Irina Lepekhina <irina.lepekhina95@gmail.com>
2019-07-24 12:38:00 +0300

    static queries

--- src/components/LatestPosts.vue
+++ src/components/LatestPosts.vue
@@ -1,10 +1,24 @@
 <template>
   <section>
     <h3>Latest Posts</h3>
-    <article>
-      <h4>Post Title</h4>
-      <p>Here is some fake text for a post body summary.</p>
+    <article v-for="edge in $static.allBlogPost.edges" :key="edge.node.id">
+      <div v-html="edge.node.content" />
+      <!-- <h4>Post Title</h4>
+      <p>Here is some fake text for a post body summary.</p> -->
       <a href="#">Read More</a>
     </article>
   </section>
 </template>
+
+<static-query>
+  query LatestPosts {
+    allBlogPost {
+      edges {
+        node {
+          id
+          content
+        }
+      }
+    }
+  }
+</static-query>
